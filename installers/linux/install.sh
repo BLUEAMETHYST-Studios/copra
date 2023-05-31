d479dd00862cbdc6d7b4b990f4311ec14e130369
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-05-31 16:59:01 +0200

    finished copra installer for linux

--- installers/linux/install.sh
+++ installers/linux/install.sh
@@ -1,1 +1,30 @@
-echo "INDEV"
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]
+then
+
+echo -e "\e[31mPlease execute as root (sudo)!\e[0m"
+exit
+
+fi
+
+read -p "Are you sure? (N/y): " confirm
+
+if [[ ${confirm} != "y" && ${confirm} != "Y" ]]; then
+    echo -e "\e[31mInstallation cancelled.\e[0m"
+    exit 1
+fi
+
+echo "STARTING"
+
+echo "Making project's directory ..."
+mkdir /usr/bin/.copra
+
+echo "Copying files ..."
+cp -a application/main. /usr/bin/.copra
+cp application/start/copra /usr/bin
+
+echo "Making Copra executable ..."
+chmod +x /usr/bin/copra
+
+echo -e "\e[32mInstallation completed!\e[0m"
